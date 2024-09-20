b466e7c477560c3f1eb351d896c772cb32c8e6dc
tek <tekrajpant224@gmail.com>
2024-09-20 10:53:43 +0545

    add nginx config

--- run.bash
+++ run.bash
@@ -4,6 +4,11 @@
 npm install -g pm2
 npm install -g npx
 
+cp admin.conf /etc/nginx/sites-available/admin
+cp frontend.conf /etc/nginx/sites-available/frontend
+ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/
+service nginx restart
+
 # Start PM2 services
 pm2 serve apps/admin/build/ 3002 --name "admin" --spa
 pm2 serve apps/ecommece-web/build/ 3001 --name "ecommece" --spa
