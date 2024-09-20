2b62a175772f630a3cf24a4a8654421addbebad7
tek <tekrajpant224@gmail.com>
2024-09-20 10:33:14 +0545

    remove comment from run.sh

--- run.bash
+++ run.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Install PM2 and npx globally
-# npm install -g pm2
-# npm install -g npx
+npm install -g pm2
+npm install -g npx
 
 # Start PM2 services
 pm2 serve apps/admin/build/ 3002 --name "admin" --spa
 pm2 serve apps/ecommece-web/build/ 3001 --name "ecommece" --spa
 
 pm2 start app.config.json
+
