ad27fbb8da1e877f69dc8d3d1d02168b18bb0a9d
tek <tekrajpant224@gmail.com>
2024-09-20 11:10:32 +0545

    add configs

--- run.bash
+++ run.bash
@@ -4,9 +4,10 @@
 npm install -g pm2
 npm install -g npx
 
-cp admin.conf /etc/nginx/sites-available/admin
-cp frontend.conf /etc/nginx/sites-available/frontend
-ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/
+cp admin /etc/nginx/sites-available/admin
+cp frontend /etc/nginx/sites-available/frontend
+ln -s /etc/nginx/sites-available/admin  /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/frontend  /etc/nginx/sites-enabled/
 service nginx restart
 
 # Start PM2 services
