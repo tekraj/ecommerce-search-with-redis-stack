f3d10038cf41e254db975b0a9293832c55628bd7
tek <tekrajpant224@gmail.com>
2024-09-21 11:26:21 +0545

    remove run server command

--- run.bash
+++ run.bash
@@ -5,7 +5,6 @@ find apps/ecommerce-web/build/ -type f -exec sed -i "s|http://localhost|http://$
 
 killall node
 pm2 stop all
-pnpm dev -F server &
 pm2 serve apps/admin/build/ 3002 --name "admin" --spa
 pm2 serve apps/ecommerce-web/build/ 3001 --name "ecommerce" --spa
 
