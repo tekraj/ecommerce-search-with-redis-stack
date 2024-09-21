70b9adcbb56b6d3012ce5c09fcc2dc55415f66ff
tek <tekrajpant224@gmail.com>
2024-09-21 11:13:58 +0545

    fix killall command

--- run.bash
+++ run.bash
@@ -3,7 +3,7 @@ pnpm initial-setup
 find apps/admin/build/ -type f -exec sed -i "s|http://localhost|http://$(curl -s ifconfig.me)|g" {} +
 find apps/ecommerce-web/build/ -type f -exec sed -i "s|http://localhost|http://$(curl -s ifconfig.me)|g" {} +
 
-node killall node
+killall node
 pm2 stop all
 pnpm dev -F server &
 pm2 serve apps/admin/build/ 3002 --name "admin" --spa
